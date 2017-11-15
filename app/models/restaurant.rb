bb89a839ec11bd030a37997cd25e6f174f3ef672
rabea bader <bdr193@gmail.com>
2017-11-15 13:55:21 +0100

    Created review models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,5 @@
 class Restaurant < ApplicationRecord
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: CATEGORIES }
 end
