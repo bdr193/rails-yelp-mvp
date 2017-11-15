960ad8c72192e61d981446139b479cfba8c21e48
rabea bader <bdr193@gmail.com>
2017-11-15 15:08:52 +0100

    added navbar without login

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   get 'reviews/create'
 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "restaurants#index"
   resources :restaurants do
     resources :reviews, only: [ :new, :create ]
   end
