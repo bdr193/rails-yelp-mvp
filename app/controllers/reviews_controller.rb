15b0e588e7eebf0b382071f0ea8519583839f894
rabea bader <bdr193@gmail.com>
2017-11-15 15:03:34 +0100

    basics work

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,25 @@
 class ReviewsController < ApplicationController
   def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
   end
 
   def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    # we need `restaurant_id` to asssociate review with corresponding restaurant
+    @review.restaurant = Restaurant.find(params[:restaurant_id])
+    @review.save
+    if @review.save
+      redirect_to restaurant_path(@review.restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   resources :restaurants do
     resources :reviews, only: [ :new, :create ]
   end
+
+  resources :reviews, only: [ :show, :edit, :update, :destroy ]
 end
