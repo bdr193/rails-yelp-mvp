bb8a1606743f88e2a9be8f0266d86e2075d96526
rabea bader <bdr193@gmail.com>
2017-11-15 14:15:51 +0100

    Reviews controller generated

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,41 +1,47 @@
 class RestaurantsController < ApplicationController
   def index
-      @restaurants = Restaurant.all
-    end
+    @restaurants = Restaurant.all
+  end
 
-    def show
-      @restaurant = Restaurant.find(params[:id])
-    end
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
 
-    def new
-      @restaurant = Restaurant.new
-    end
+  def new
+    @restaurant = Restaurant.new
+  end
 
-    def create
-      @restaurant = Restaurant.new(restaurant_params)
-      @restaurant.save
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
       redirect_to @restaurant
+    else
+      render :new
     end
+  end
 
-    def edit
-      @restaurant = Restaurant.find(params[:id])
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :edit
     end
+  end
 
-    def update
-      @restaurant = Restaurant.find(params[:id])
-      @restaurant.update(restaurant_params)
-      redirect_to restaurant_path
-    end
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_path
+  end
 
-    def destroy
-      @restaurant = Restaurant.find(params[:id])
-      @restaurant.destroy
-      redirect_to restaurants_path
-    end
+  private
 
-    private
-
-    def restaurant_params
-      params.require(:restaurant).permit(:name, :address, :phone_number, :category)
-    end
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,5 @@ class Restaurant < ApplicationRecord
   CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
   validates :name, :address, :category, presence: true
   validates :category, inclusion: { in: CATEGORIES }
+  has_many :reviews, dependent: :destroy
 end
